6206cf4eff483082752549a1f8a77cb485202f11
hiisyaam <farishiisyamhardimann@gmail.com>
2026-07-18 22:21:06 +0700

    update rice box logo

--- resources/views/armada/index.blade.php
+++ resources/views/armada/index.blade.php
@@ -279,8 +279,10 @@
 
                             @if($k->mini_bar)
                             <div class="w-20 h-24 sm:w-28 sm:h-28 flex flex-col items-center justify-center p-2 bg-white/5 rounded-2xl hover:bg-white/10 hover:-translate-y-1 transition-all border border-transparent hover:border-white/10 group">
-                                <span class="material-icons-round text-2xl sm:text-3xl text-gray-300 group-hover:text-white mb-1 sm:mb-2">local_bar</span>
-                                <span class="text-[9px] sm:text-[10px] font-bold font-sans uppercase tracking-wider text-gray-400 group-hover:text-kkpBlue text-center leading-tight px-1">Mini<br>Bar</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 sm:w-8 sm:h-8 text-gray-300 group-hover:text-white mb-1 sm:mb-2 transition-colors">
+                                    <path d="M20 3H6c-1.1 0-2 .9-2 2v8c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m0 5h-2V5h2zM3 21h16c.55 0 1-.45 1-1s-.45-1-1-1H3c-.55 0-1 .45-1 1s.45 1 1 1"/>
+                                </svg>
+                                <span class="text-[9px] sm:text-[10px] font-bold font-sans uppercase tracking-wider text-gray-400 group-hover:text-kkpBlue text-center leading-tight px-1">Mini<br>Pantry</span>
                             </div>
                             @endif
 
@@ -307,7 +309,9 @@
 
                             @if($k->rice_box)
                             <div class="w-20 h-24 sm:w-28 sm:h-28 flex flex-col items-center justify-center p-2 bg-white/5 rounded-2xl hover:bg-white/10 hover:-translate-y-1 transition-all border border-transparent hover:border-white/10 group">
-                                <span class="material-icons-round text-2xl sm:text-3xl text-gray-300 group-hover:text-white mb-1 sm:mb-2">lunch_dining</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 sm:w-8 sm:h-8 text-gray-300 group-hover:text-white mb-1 sm:mb-2 transition-colors">
+                                    <path d="M22 12c0-5.48-4.4-9.93-9.86-10-3.62-.05-6.85 2.03-8.71 5.14C.1 12.69 2.98 18.27 8 20.25v.25c0 .83.67 1.5 1.5 1.5h5c.83 0 1.5-.67 1.5-1.5v-.25c3.53-1.39 6-4.56 6-8.25m-2 0h-4V5.08c2.39 1.39 4 3.97 4 6.92m-6-7.74V12h-4V4.26c.64-.16 1.31-.26 2-.26s1.36.1 2 .26M4 12c0-2.95 1.61-5.53 4-6.92V12z"/>
+                                </svg>
                                 <span class="text-[8px] sm:text-[10px] font-bold font-sans uppercase tracking-wider text-gray-400 group-hover:text-kkpBlue text-center leading-tight px-1">Rice<br>Box</span>
                             </div>
                             @endif
